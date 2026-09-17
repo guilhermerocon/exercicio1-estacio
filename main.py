51555d758084abb64d81380906db0ddbc61f619c
guilhermerocon <guilhermerocon1234445@gmail.com>
2026-09-17 20:42:03 -0300

    Subindo respostas

---
+++ main.py
@@ -0,0 +1,9 @@
+#11)
+nome = input("Digite seu nome: ")
+print(f"Olá, {nome}!")
+
+#12)
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+soma = numero1 + numero2
+print(f"A soma de {numero1} e {numero2} é {soma}.")
